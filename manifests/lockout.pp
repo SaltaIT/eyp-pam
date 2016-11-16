7d36b7f2c59fd7110f29eeeec7e8dd3bc7d04dfa
Jordi Prats <jprats@systemadmin.es>
2016-11-16 14:42:24 +0100

    rename

--- manifests/lockout.pp
+++ manifests/lockout.pp
@@ -1,5 +1,10 @@
+# faillock
 # 4.1.2 - https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/7/html/Security_Guide/chap-Hardening_Your_System_with_Tools_and_Services.html#sect-Security_Guide-Workstation_Security-Account_Locking
-class pam::faillock(
+#
+# pam_tally2
+# http://myexperienceswithunix.blogspot.com.es/2016/09/locking-user-accounts-with-pam-faillock.html
+#
+class pam::lockout(
                       $manage_package        = true,
                       $package_ensure        = 'installed',
                       $manage_service        = true,
@@ -11,8 +16,8 @@ class pam::faillock(
   include ::pam
 
   Class['::pam'] ->
-  class { '::pam::faillock::install': } ->
-  class { '::pam::faillock::config': } ->
-  Class['::pam::faillock']
+  class { '::pam::lockout::install': } ->
+  class { '::pam::lockout::config': } ->
+  Class['::pam::lockout']
 
 }
