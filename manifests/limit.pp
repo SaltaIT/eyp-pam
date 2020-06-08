1ca135a2579d8d06c1029f5e7afbfb0db43e2f78
Jordi Prats <jordi.prats@gmail.com>
2020-06-08 15:41:54 +0200

    lint

--- manifests/limit.pp
+++ manifests/limit.pp
@@ -1,6 +1,9 @@
-define pam::limit ( $item, $value, $domain=$name, $type='-' ) {
-
-  #
+define pam::limit (
+                    $item,
+                    $value,
+                    $domain=$name,
+                    $type='-'
+                  ) {
   concat::fragment{ "limits.conf ${domain} ${name} ${type} ${item} ${value}":
     target  => $pam::params::limits_conf,
     order   => '01',
